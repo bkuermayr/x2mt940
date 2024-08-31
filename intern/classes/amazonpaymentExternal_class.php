50cd76378d815cd4dc262896096d0b8c868cd04c
bkuermayr <benjamin@kuermayr.com>
2024-08-31 22:47:39 +0200

    tes

--- intern/classes/amazonpaymentExternal_class.php
+++ intern/classes/amazonpaymentExternal_class.php
@@ -2,42 +2,40 @@
 
 class amazonPayExternal {
 
-	private $mt940param;
-	private	$data;
-	private $inFile;
-	private $ppHeader;
-	private $amountTotal;
-	private $dataPos;
-	private $dataCount;
-	private $ppcodes;
-	private $mapping;
-	
-	public function __construct($fileName) {
+    private $mt940param;
+    private $data;
+    private $inFile;
+    private $ppHeader;
+    private $amountTotal;
+    private $dataPos;
+    private $dataCount;
+    private $mapping;
 
-		include './intern/config.php';
+    public function __construct($fileName) {
+        include './intern/config.php';
 
-		// initialize variables
-		$this->mt940param = $amazon;
-		$this->data = [];
-		$this->amountTotal = 0;
-		$this->dataPos = 0;
-		$this->dataCount = 0;
-		
-		$this->infile = new myfile($fileName);
+        // Initialize variables
+        $this->mt940param = $amazon ?? []; // Ensure $amazon is an array
+        $this->data = [];
+        $this->amountTotal = 0;
+        $this->dataPos = 0;
+        $this->dataCount = 0;
 
-		if (file_exists("./intern/mapping/".$mapping_prefix."amazonpayExternal.json")) {
-			$mapping = new myfile("./intern/mapping/".$mapping_prefix."amazonpayExternal.json","readfull");
-		} else {
-			$mapping = new myfile("./intern/mapping/amazonpayExternal.json","readfull");
-		}
-		$this->mapping = $mapping->readJson();
+        $this->inFile = new myfile($fileName); // Changed to use inFile property
 
-		$this->mt940param['startdate'] = null;
-		$this->mt940param['enddate'] = null;
+        if (file_exists("./intern/mapping/".$mapping_prefix."amazonpayExternal.json")) {
+            $mapping = new myfile("./intern/mapping/".$mapping_prefix."amazonpayExternal.json", "readfull");
+        } else {
+            $mapping = new myfile("./intern/mapping/amazonpayExternal.json", "readfull");
+        }
+        $this->mapping = $mapping->readJson();
 
-		// Read the CSV header
+        $this->mt940param['startdate'] = null;
+        $this->mt940param['enddate'] = null;
+
+        // Read the CSV header
         $headerRead = false;
-        while (($row = $this->infile->readCSV(',')) !== false) {
+        while (($row = $this->inFile->readCSV(',')) !== false) { // Use inFile property
             if (!$headerRead) {
                 // Remove BOM and quotes
                 if (!empty($row[0])) {
@@ -58,169 +56,179 @@ class amazonPayExternal {
 
         // Output the final header after setting it
         echo "<pre>Final Header: " . json_encode($this->ppHeader, JSON_PRETTY_PRINT) . "</pre>";
-	}
-	
-	public function importData() {
-		
-		while (($row = $this->infile->readCSV(',')) !== false) {
-			echo "<pre>Row Import Data: " . json_encode($row, JSON_PRETTY_PRINT) . "</pre>";
+    }
 
-			$rowdata = [];
-			$rowdata = array_combine($this->ppHeader,$row);
-			if ($this->mt940param['startdate'] == null) {
-				$this->mt940param['startdate']	= $rowdata[$this->mapping['TRANSACTION_DATE']];
-			}
-			$rowdata[$this->mapping['TRANSACTION_AMOUNT']] = str_replace(".","",$rowdata[$this->mapping['TRANSACTION_AMOUNT']]);
-			$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = str_replace(".","",$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
+    public function importData() {
+        
+        while (($row = $this->inFile->readCSV(',')) !== false) { // Use inFile property
+            echo "<pre>Row Import Data: " . json_encode($row, JSON_PRETTY_PRINT) . "</pre>";
 
-			$rowdata[$this->mapping['TRANSACTION_AMOUNT']] = str_replace(",",".",$rowdata[$this->mapping['TRANSACTION_AMOUNT']]);
-			$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = str_replace(",",".",$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
+            $rowdata = [];
+            $rowdata = array_combine($this->ppHeader, $row);
+            if ($this->mt940param['startdate'] == null) {
+                $this->mt940param['startdate'] = date("ymd", strtotime($rowdata[$this->mapping['TRANSACTION_DATE']] ?? '')); // Added null coalescing operator
+            }
+            $rowdata[$this->mapping['TRANSACTION_AMOUNT']] = str_replace(".", "", $rowdata[$this->mapping['TRANSACTION_AMOUNT']] ?? '0'); // Added null coalescing operator
+            $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = str_replace(".", "", $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] ?? '0'); // Added null coalescing operator
 
-			if (! in_array($rowdata[$this->mapping['TRANSACTION_EVENTCODE']], $this->mapping['CHECK_EXCLUDECODE'])) {
+            $rowdata[$this->mapping['TRANSACTION_AMOUNT']] = str_replace(",", ".", $rowdata[$this->mapping['TRANSACTION_AMOUNT']] ?? '0'); // Added null coalescing operator
+            $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = str_replace(",", ".", $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] ?? '0'); // Added null coalescing operator
 
-				if ($rowdata[$this->mapping['TRANSACTION_AMOUNT']] > 0) {
-					$transactionType = "C";
-					$transactionChargeType = "D";
-					$this->amountTotal += $rowdata[$this->mapping['TRANSACTION_AMOUNT']];
-					$this->amountTotal += $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']];
+            if (!empty($rowdata[$this->mapping['TRANSACTION_EVENTCODE']]) && !in_array($rowdata[$this->mapping['TRANSACTION_EVENTCODE']], $this->mapping['CHECK_EXCLUDECODE'])) {
+                // Check if TRANSACTION_EVENTCODE exists and is not in CHECK_EXCLUDECODE
 
-					$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
-				} else {
-					$transactionType = "D";
-					$transactionChargeType = "C";
-					$rowdata[$this->mapping['TRANSACTION_AMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_AMOUNT']]);
-					$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
+                if ($rowdata[$this->mapping['TRANSACTION_AMOUNT']] > 0) {
+                    $transactionType = "C";
+                    $transactionChargeType = "D";
+                    $this->amountTotal += $rowdata[$this->mapping['TRANSACTION_AMOUNT']];
+                    $this->amountTotal += $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']];
 
-					$this->amountTotal -= $rowdata[$this->mapping['TRANSACTION_AMOUNT']];
-					$this->amountTotal -= $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']];
-				}
-			
-				$name = strtoupper(preg_replace( '/[^a-z0-9 ]/i', '_', $rowdata[$this->mapping['TRANSACTION_SELLER_NAME']]));
+                    $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
+                } else {
+                    $transactionType = "D";
+                    $transactionChargeType = "C";
+                    $rowdata[$this->mapping['TRANSACTION_AMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_AMOUNT']]);
+                    $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']] = abs($rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']]);
 
-				$fromDate = date("Y-m-d",strtotime($rowdata[$this->mapping['ORDER_DATE']]));
-				$toDate = date("Y-m-d",time());
-				
-				if ($rowdata[$this->mapping['TRANSACTION_EVENTCODE']] == $this->mapping['CHECK_CANCEL_PAYMENT']) {
-					$ppid = $rowdata[$this->mapping['TRANSACTION_ORIGINAL_CODE']];
-				} else {
-					$ppid = $rowdata[$this->mapping['TRANSACTION_CODE']];
-				}
-				
-				$invoiceData = $this->wwsInvoices->getInvoiceData($ppid, $fromDate, $toDate, $this->mt940param['fromCustomer'], $this->mt940param['toCustomer']);
-				
-				$defaultInvoice = 'NONREF';
-				$defaultCustomer = 'AMAZON_CUSTOMER';
-				$invoiceStr = 'NONREF';
-			
-				$spacePos = strpos($rowdata[$this->mapping['TRANSACTION_EVENTCODE']]," ",5);
-				if (! $spacePos) { $spacePos = 30; }
-				$event = substr($rowdata[$this->mapping['TRANSACTION_EVENTCODE']],0,$spacePos);
-				if ($event == $this->mapping['TRANSACTION_EVENTCODE']) {
-					$event .= " PAYOUT";
-				}
+                    $this->amountTotal -= $rowdata[$this->mapping['TRANSACTION_AMOUNT']];
+                    $this->amountTotal -= $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']];
+                }
 
-			
-				$mt940 = [];
-				
-//				if (($rowdata[$this->mapping['TRANSACTION_AMOUNT']] <> 0) and ($rowdata[$this->mapping['TRANSACTION_EVENTCODE']] == $this->mapping["CHECK_FINISH_STAT"])) {
-				if ($rowdata[$this->mapping['TRANSACTION_AMOUNT']] <> 0) {
-					echo "<pre>Mapping Transaction amount not zero: " . json_encode($rowdata, JSON_PRETTY_PRINT) . "</pre>";
-					
-					$mt940 = [
-						'PAYMENT_DATE' => date("ymd",strtotime($rowdata[$this->mapping['TRANSACTION_DATE']])),
-						'PAYMENT_TYPE' => $transactionType,
-						'PAYMENT_AMOUNT' => str_replace(".",",",sprintf("%01.2f",$rowdata[$this->mapping['TRANSACTION_AMOUNT']])),
-						'PAYMENT_NDDT' => $defaultInvoice,
-						'PAYMENT_TEXT00' => 'AMAZON',
-						'PAYMENT_TEXT20' => 'AMAZON '.$defaultCustomer,
-						'PAYMENT_TEXT21' => $invoiceStr,
-						'PAYMENT_TEXT22' => $rowdata[$this->mapping['TRANSACTION_CODE']],
-						'PAYMENT_TEXT23' => $event." ".strtoupper($name),
-						'PAYMENT_CODE' => $event,
-						'CHARGE_DATE' => date("ymd",strtotime($rowdata[$this->mapping['TRANSACTION_DATE']])),
-						'CHARGE_TYPE' => $transactionChargeType,
-						'CHARGE_AMOUNT' => str_replace(".",",",sprintf("%01.2f",$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']])),
-						'CHARGE_NDDT' => 'NONREF',
-						'CHARGE_TEXT00' => 'AMAZON',
-						'CHARGE_TEXT20' => 'AMAZON GEB.',
-						'CHARGE_TEXT21' => $rowdata[$this->mapping['TRANSACTION_CODE']],
-						'CHARGE_TEXT22' => strtoupper($name),
-						'PAYMENT_STATE' =>  'S'
-					];
-				} 
-				
-				$this->data[] = $mt940;
-				
-				$this->dataCount++;
-			}
+                $name = strtoupper(preg_replace('/[^a-z0-9 ]/i', '_', $rowdata[$this->mapping['TRANSACTION_SELLER_NAME']] ?? 'UNKNOWN')); // Added null coalescing operator
 
-			$this->mt940param['enddate'] = $rowdata[$this->mapping['TRANSACTION_DATE']];
+                $event = substr($rowdata[$this->mapping['TRANSACTION_EVENTCODE']], 0, 30);
+                if ($event == $this->mapping['TRANSACTION_EVENTCODE']) {
+                    $event .= " PAYOUT";
+                }
 
-		}	
-		
-		if (($this->mt940param['payout']) ) {
-			$this->createPayoutData($this->amountTotal, $this->mt940param['enddate']);
-		}
-		
-		if ($this->amountTotal < 0) {
-			$SH = "D";
-			$this->amountTotal = $this->amountTotal * (-1);
-		} else {
-			$SH = "C";
-			$this->amountTotal = $this->amountTotal;
-		}
-		 $this->mt940param["TotalAmount"] = $this->amountTotal;
-		 $this->mt940param["TotalSH"] = $SH;
-	}
-	
-	public function getAllData() {
-		return $this->data;
-	}
-	
-	public function getNext() {
-		if ($this->dataPos < $this->dataCount) {
-			return $this->data[$this->dataPos++];
-		} else {
-			return false;
-		}
-	}
-	
-	public function getParameter() {
-		return $this->mt940param;
-	}
-	
-	private function createPayoutData($sumOfDay,$payoutdate) {
-		if ($sumOfDay < 0) {
-			$sumOfDay = abs($sumOfDay);
-			$type = 'D';
-		} else {
-			$type = 'C';
-		}
-		
-		$mt940 = [
-				'PAYMENT_DATE' => date("ymd",strtotime($payoutdate)),
-				'PAYMENT_TYPE' => $type,
-				'PAYMENT_AMOUNT' => str_replace(".",",",sprintf("%01.2f",$sumOfDay)),
-				'PAYMENT_NDDT' => '',
-				'PAYMENT_TEXT00' => 'AMAZON',
-				'PAYMENT_TEXT20' => 'AMAZON PAYOUT '.$payoutdate,
-				'PAYMENT_TEXT21' => '',
-				'PAYMENT_TEXT22' => '',
-				'PAYMENT_TEXT23' => '',
-				'PAYMENT_CODE' => 'PayOut',
-				'CHARGE_DATE' => '',
-				'CHARGE_TYPE' => '',
-				'CHARGE_AMOUNT' => '',
-				'CHARGE_NDDT' => '',
-				'CHARGE_TEXT00' => '',
-				'CHARGE_TEXT20' => '',
-				'CHARGE_TEXT21' => '',
-				'CHARGE_TEXT22' => '',
-				'PAYMENT_STATE' =>  'S'
-		];
-		$this->data[] = $mt940;
-	}
-	
+                // Create MT940 transaction entry
+                $mt940 = [];
+
+                if ($rowdata[$this->mapping['TRANSACTION_AMOUNT']] <> 0) {
+                    echo "<pre>Mapping Transaction amount not zero: " . json_encode($rowdata, JSON_PRETTY_PRINT) . "</pre>";
+
+                    $mt940 = [
+                        'PAYMENT_DATE' => date("ymd", strtotime($rowdata[$this->mapping['TRANSACTION_DATE']] ?? '')), // Added null coalescing operator
+                        'PAYMENT_TYPE' => $transactionType,
+                        'PAYMENT_AMOUNT' => str_replace(".", ",", sprintf("%01.2f", $rowdata[$this->mapping['TRANSACTION_AMOUNT']])),
+                        'PAYMENT_NDDT' => 'NONREF',
+                        'PAYMENT_TEXT00' => 'AMAZON',
+                        'PAYMENT_TEXT20' => 'AMAZON AMAZON_CUSTOMER',
+                        'PAYMENT_TEXT21' => 'NONREF',
+                        'PAYMENT_TEXT22' => $rowdata[$this->mapping['TRANSACTION_CODE']] ?? '', // Added null coalescing operator
+                        'PAYMENT_TEXT23' => $event . " " . strtoupper($name),
+                        'PAYMENT_CODE' => $event,
+                        'CHARGE_DATE' => date("ymd", strtotime($rowdata[$this->mapping['TRANSACTION_DATE']] ?? '')), // Added null coalescing operator
+                        'CHARGE_TYPE' => $transactionChargeType,
+                        'CHARGE_AMOUNT' => str_replace(".", ",", sprintf("%01.2f", $rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']])),
+                        'CHARGE_NDDT' => 'NONREF',
+                        'CHARGE_TEXT00' => 'AMAZON',
+                        'CHARGE_TEXT20' => 'AMAZON GEB.',
+                        'CHARGE_TEXT21' => $rowdata[$this->mapping['TRANSACTION_CODE']] ?? '', // Added null coalescing operator
+                        'CHARGE_TEXT22' => strtoupper($name),
+                        'PAYMENT_STATE' => 'S'
+                    ];
+
+                    $this->data[] = $mt940;
+                    $this->dataCount++;
+                }
+            }
+
+            $this->mt940param['enddate'] = date("ymd", strtotime($rowdata[$this->mapping['TRANSACTION_DATE']] ?? '')); // Added null coalescing operator
+        }
+
+        // Check if payout is defined before using it
+        if (!empty($this->mt940param['payout'])) {
+            $this->createPayoutData($this->amountTotal, $this->mt940param['enddate']);
+        }
+
+        if ($this->amountTotal < 0) {
+            $SH = "D";
+            $this->amountTotal = $this->amountTotal * (-1);
+        } else {
+            $SH = "C";
+        }
+        $this->mt940param["TotalAmount"] = $this->amountTotal;
+        $this->mt940param["TotalSH"] = $SH;
+    }
+
+    public function getAllData() {
+        return $this->data;
+    }
+
+    public function getNext() {
+        if ($this->dataPos < $this->dataCount) {
+            return $this->data[$this->dataPos++];
+        } else {
+            return false;
+        }
+    }
+
+    public function getParameter() {
+        return $this->mt940param;
+    }
+
+    private function createPayoutData($sumOfDay, $payoutdate) {
+        if ($sumOfDay < 0) {
+            $sumOfDay = abs($sumOfDay);
+            $type = 'D';
+        } else {
+            $type = 'C';
+        }
+
+        $mt940 = [
+            'PAYMENT_DATE' => date("ymd", strtotime($payoutdate)),
+            'PAYMENT_TYPE' => $type,
+            'PAYMENT_AMOUNT' => str_replace(".", ",", sprintf("%01.2f", $sumOfDay)),
+            'PAYMENT_NDDT' => '',
+            'PAYMENT_TEXT00' => 'AMAZON',
+            'PAYMENT_TEXT20' => 'AMAZON PAYOUT ' . $payoutdate,
+            'PAYMENT_TEXT21' => '',
+            'PAYMENT_TEXT22' => '',
+            'PAYMENT_TEXT23' => '',
+            'PAYMENT_CODE' => 'PayOut',
+            'CHARGE_DATE' => '',
+            'CHARGE_TYPE' => '',
+            'CHARGE_AMOUNT' => '',
+            'CHARGE_NDDT' => '',
+            'CHARGE_TEXT00' => '',
+            'CHARGE_TEXT20' => '',
+            'CHARGE_TEXT21' => '',
+            'CHARGE_TEXT22' => '',
+            'PAYMENT_STATE' => 'S'
+        ];
+        $this->data[] = $mt940;
+    }
+
+    public function generateMT940() {
+        $mt940 = [];
+
+        // Set initial balance
+        $mt940[] = ":20:" . "AMZ" . date("ymdHis");
+        $mt940[] = ":25:" . $this->mt940param['blz'] . "/" . $this->mt940param['konto'];
+        $mt940[] = ":28C:00000";
+        $mt940[] = ":60F:" . $this->mt940param['TotalSH'] . $this->mt940param['startdate'] . $this->mt940param['currency'] . "0,00";
+
+        foreach ($this->data as $transaction) {
+            $transactionEntry = ":61:" . $transaction['PAYMENT_DATE'] . $transaction['PAYMENT_TYPE'] . $transaction['PAYMENT_AMOUNT'] . "NMSC" . $transaction['PAYMENT_CODE'];
+            $descriptionEntry = ":86:" . $transaction['PAYMENT_TEXT00'] . " " . $transaction['PAYMENT_TEXT20'] . " " . $transaction['PAYMENT_TEXT22'];
+
+            // Debug: Output each transaction and description entry
+            echo "<pre>Transaction Entry: $transactionEntry</pre>";
+            echo "<pre>Description Entry: $descriptionEntry</pre>";
+
+            $mt940[] = $transactionEntry;
+            $mt940[] = $descriptionEntry;
+        }
+
+        // Add end balance
+        $mt940[] = ":62F:" . $this->mt940param['TotalSH'] . $this->mt940param['enddate'] . $this->mt940param['currency'] . $this->mt940param['TotalAmount'];
+
+        // Debug: Output MT940 footer
+        echo "<pre>MT940 Footer: " . end($mt940) . "</pre>";
+
+        return implode("\r\n", $mt940);
+    }
 }
 
 ?>
